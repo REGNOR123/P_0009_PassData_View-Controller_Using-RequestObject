531f4502095e61f5b22e1a172667823fa3d95265
Amit Kumar <81771413+REGNOR123@users.noreply.github.com>
2026-01-18 10:57:10 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P_0009_PassData_View_Controller_Using_RequestObject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
